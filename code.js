dc0c8e2491ff1e4da217fe12554fc33ee58857f3
Edson <edc5fc@gmail.com>
2023-04-25 19:39:57 -0700

    fixed the arrow key movement system (YAY)

--- code.js
+++ code.js
@@ -1,27 +1,30 @@
 
 //using arrow keys to move player 
-//FIXME (left arrow doesn't work, and also sometimes the up arrow)
+
+document.getElementById('player-image').style.position = "absolute";
 playerMovementSpeed = 5;
 document.onkeydown = detectKey;
 function detectKey(e) {
     var posLeft = document.getElementById('player-image').offsetLeft;
     var posTop = document.getElementById('player-image').offsetTop;
     e = e || window.event;
+
+    console.log(e.keyCode)
     if (e.keyCode == '38') {
         // up arrow
-        document.getElementById('player-image').style.marginTop  = (posTop-playerMovementSpeed)+"px";
+        document.getElementById('player-image').style.top  = (posTop-playerMovementSpeed)+"px";
     }
     else if (e.keyCode == '40') {
         // down arrow
-        document.getElementById('player-image').style.marginTop  = (posTop+playerMovementSpeed)+"px";
+        document.getElementById('player-image').style.top  = (posTop+playerMovementSpeed)+"px";
     }
     else if (e.keyCode == '37') {
        // left arrow
-        document.getElementById('player-image').style.marginLeft  = (posLeft-playerMovementSpeed)+"px";
+        document.getElementById('player-image').style.left  = (posLeft-playerMovementSpeed)+"px";
     }
     else if (e.keyCode == '39') {
        // right arrow
-        document.getElementById('player-image').style.marginLeft  = (posLeft+playerMovementSpeed)+"px";
+        document.getElementById('player-image').style.left  = (posLeft+playerMovementSpeed)+"px";
     }
-    console.log(playerMovementSpeed, document.getElementById('player-image').style.marginTop, document.getElementById('player-image').style.marginLeft)
+    console.log(playerMovementSpeed, document.getElementById('player-image').style.top, document.getElementById('player-image').style.left)
 }
