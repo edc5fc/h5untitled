4d3d3d847eb958779bbc2b2787835e5b43c2847a
Edson <edc5fc@gmail.com>
2023-04-25 19:03:27 -0700

    Added arrow controls, but the left arrow does not work (FIXME)

--- code.js
+++ code.js
@@ -1,10 +1,27 @@
-let testVariable = 3;
-document.getElementById("test").innerHTML = testVariable;
 
-const button = document.getElementById("clicky");
-button.addEventListener('click', GetMoney)
-function GetMoney(){
-    testVariable++;
-    console.log("YAS")
-    document.getElementById("test").innerHTML = testVariable;
+//using arrow keys to move player 
+//FIXME (left arrow doesn't work, and also sometimes the up arrow)
+playerMovementSpeed = 5;
+document.onkeydown = detectKey;
+function detectKey(e) {
+    var posLeft = document.getElementById('player-image').offsetLeft;
+    var posTop = document.getElementById('player-image').offsetTop;
+    e = e || window.event;
+    if (e.keyCode == '38') {
+        // up arrow
+        document.getElementById('player-image').style.marginTop  = (posTop-playerMovementSpeed)+"px";
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+        document.getElementById('player-image').style.marginTop  = (posTop+playerMovementSpeed)+"px";
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+        document.getElementById('player-image').style.marginLeft  = (posLeft-playerMovementSpeed)+"px";
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+        document.getElementById('player-image').style.marginLeft  = (posLeft+playerMovementSpeed)+"px";
+    }
+    console.log(playerMovementSpeed, document.getElementById('player-image').style.marginTop, document.getElementById('player-image').style.marginLeft)
 }
