ea849e337f31a595dbd756e8def45a694317fbb3
Edson <edc5fc@gmail.com>
2023-06-13 20:08:43 -0700

    deleted player movement controls, added CSS, and changed the text display function. #FIXME, not working properly.

--- code.js
+++ code.js
@@ -1,47 +1,16 @@
-
-//using arrow keys to move player 
-document.getElementById('player-image').style.position = "absolute";
-playerMovementSpeed = 5;
-document.onkeydown = detectKey;
-function detectKey(e) {
-    var posLeft = document.getElementById('player-image').offsetLeft;
-    var posTop = document.getElementById('player-image').offsetTop;
-    e = e || window.event;
-
-    console.log(e.keyCode)
-    if (e.keyCode == '38') {
-        // up arrow
-        document.getElementById('player-image').style.top  = (posTop-playerMovementSpeed)+"px";
+function displayText(text){
+    let visibleText = ""
+    for(let i = 0; i < text.length; i++){
+        visibleText = visibleText + text[i]
+        const NextLetter = setTimeout(NextLetter, 50)
+        function NextLetter(){
+            document.getElementById("progress-text").innerHTML = visibleText;}
+         
     }
-    else if (e.keyCode == '40') {
-        // down arrow
-        document.getElementById('player-image').style.top  = (posTop+playerMovementSpeed)+"px";
-    }
-    else if (e.keyCode == '37') {
-       // left arrow
-        document.getElementById('player-image').style.left  = (posLeft-playerMovementSpeed)+"px";
-    }
-    else if (e.keyCode == '39') {
-       // right arrow
-        document.getElementById('player-image').style.left  = (posLeft+playerMovementSpeed)+"px";
-    }
-    console.log(playerMovementSpeed, document.getElementById('player-image').style.top, document.getElementById('player-image').style.left)
-}
-function displayCharacterSpeech(speech){
-    let displaySpeech = '';
-    const displaySpeechArray = [];
     
-    for(let i = 0; i < speech.length; i++){
-        displaySpeech = displaySpeech.concat(speech[i]);
-        displaySpeechArray.push(displaySpeech);
-        setTimeout (() => {
-            document.getElementById('speechbox').innerHTML = displaySpeechArray[i];
-        }, i * 75); 
-    
+        
 }
-
-
-
    
-}
-displayCharacterSpeech("~print out da message dude, this is for testing :)")
+
+
+displayText("line 1 <br><br> line 2")
