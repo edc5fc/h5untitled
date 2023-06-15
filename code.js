2f5027077a7a0e672df99c2ae18d16b3d2726e82
Edson <edc5fc@gmail.com>
2023-06-14 19:43:19 -0700

    fixed the text display

--- code.js
+++ code.js
@@ -1,10 +1,11 @@
 function displayText(text){
     let visibleText = ""
+    const visibleTextArray = []
     for(let i = 0; i < text.length; i++){
-        visibleText = visibleText + text[i]
-        const NextLetter = setTimeout(NextLetter, 50)
-        function NextLetter(){
-            document.getElementById("progress-text").innerHTML = visibleText;}
+        visibleText = visibleText.concat(text[i])
+        visibleTextArray.push(visibleText)
+        setTimeout(() => {
+            document.getElementById("progress-text").innerHTML = visibleTextArray[i]}, i * 50)
          
     }
     
