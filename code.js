36069de844606cd3bb821e0fe10cc7f507d3c2b4
Edson <edc5fc@gmail.com>
2023-06-20 20:11:17 -0700

    made display text work multiple times

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
-function displayText(text){
+function displayText(text, delay = 0){
     let visibleText = ""
     const visibleTextArray = []
     for(let i = 0; i < text.length; i++){
         visibleText = visibleText.concat(text[i])
         visibleTextArray.push(visibleText)
         setTimeout(() => {
-            document.getElementById("progress-text").innerHTML = visibleTextArray[i]}, i * 50)
+            document.getElementById("progress-text").innerHTML = visibleTextArray[i]}, delay + i * 100)
          
     }
     
@@ -13,6 +13,14 @@ function displayText(text){
 }
    
 
-const textArray = ["one day, joe mama said, <br><br> that poo is holy. <br><br>", "Then, Joe the green ate a moss and died.", "<br><br>Next, Sadfire grilled the toast of holiness, and ate it."]
+const textArray = ["one day, joe mama said, <br><br> that poo is holy. <br><br> Joe the Epik, knowing of this from the divine yumminess of the zapperwok, wanted to destroy it. <br><br> So begins the Epic Saga Of Joe.", "Setting out to find the holiest poo, Hammurabi killed a duck to reaveal the poo that would destroy the world. <br> <br>Then, Joe the green ate a moss and died.", "<br><br>Next, Sadfire grilled the toast of holiness, and ate it."]
 
-displayText(textArray[0] + textArray[1] + textArray[2])
+let delay = 0;
+let offset = 7000;
+
+displayText(textArray[0]);
+
+delay = textArray[1].length * 100 + offset;
+
+displayText(textArray[1], delay)
+
